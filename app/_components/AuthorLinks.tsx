ba0335e50a83dda21b714ea0f7679a8d39146bc9
dentonzh <denzix@gmail.com>
2023-11-13 15:43:33 -0500

    Fix bug in AuthorLinks

--- app/_components/AuthorLinks.tsx
+++ app/_components/AuthorLinks.tsx
@@ -12,7 +12,12 @@ const authorLinks = async () => {
     <div className="flex flex-col w-full sm:w-1/2 mt-3">
       <div className="font-medium">Links</div>
       {Object.keys(links).filter(key => key.startsWith('link') && links[key].length ).map(key => {
-        const name = links[`nameFor${key.replace('link', '')}`] && links[`nameFor${key.replace('link', '')}`].length ? links[`nameFor${key.replace('link', '')}`] : key.replace('link', '')
+        let name
+        if (key.startsWith('linkToWebsite')) {
+          name = links[`nameFor${key.replace('linkTo', '')}`] && links[`nameFor${key.replace('linkTo', '')}`].length ? links[`nameFor${key.replace('linkTo', '')}`] : key.replace('linkTo', '')
+        } else {
+          name = links[`nameFor${key.replace('link', '')}`] && links[`nameFor${key.replace('link', '')}`].length ? links[`nameFor${key.replace('link', '')}`] : key.replace('link', '')
+        }
 
         return (
           <div key={key}>
