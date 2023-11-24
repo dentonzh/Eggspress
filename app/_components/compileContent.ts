a0b52ed00d26472b950fb4aa2ae5fe1fb13ef0c9
dentonzh <denzix@gmail.com>
2023-11-24 17:19:57 -0500

    Rollback changes made to compileContent.ts

--- app/_components/compileContent.ts
+++ app/_components/compileContent.ts
@@ -5,36 +5,15 @@ import rehypeSlug from 'rehype-slug'
 import remarkGfm from 'remark-gfm'
 import eggspressMedia from '@/plugins/eggspress-img-processor'
 import { ImageFile, OGImage } from '@/types/Blog'
-import { getFolders } from '../utils'
-import dynamic from 'next/dynamic'
 
 const fs = require('fs-extra')
 const sizeOf = require('image-size')
 
 const videoExtensions = ['.webm', '.mp4', '.m4v', '.mov', '.wmv', '.asf', '.avi', '.mpg', '.mpeg']
 
-async function importComponents(componentNames: string[]) {
-  return componentNames.map(name => {
-    return dynamic(() => import(`../_plugins/${name}/${name}`))
-  })
-}
-
 const compileContent = async (type: string, slug:string,): Promise<{content: React.ReactNode, frontmatter: Record<any, any>, contentLength: number, images: OGImage[]}> => {
   const { markdownData, imageFiles, filePath } = await getContent(type, slug)
-  const plugins = await getFolders(`./app/_plugins/`)
-  const components = await importComponents(plugins)
-
-  Promise.all(components).then(c => console.log(c))
   
-  const componentObjects = await Promise.all(components).then(loadedComponents => {
-    const componentsObject = loadedComponents.reduce((obj: any, component: any, index: any) => {
-      obj[plugins[index]] = component.default
-      return obj
-    }, {})
-    return componentsObject
-  })
-  
-  console.log(componentObjects)
   const source = await compileMDX({
     source: markdownData,
     options: {
@@ -45,8 +24,7 @@ const compileContent = async (type: string, slug:string,): Promise<{content: Rea
       }
     },
     components: {
-      img: EggspressImage as any,
-      ...componentObjects
+      img: EggspressImage as any
     }
   })
 
