0602373e5201ad841d9fd0f122d46b994a857d62
dentonzh <denzix@gmail.com>
2023-11-23 23:35:18 -0500

    Add startIndex to blog pagination page; check boundary to ensure that max page from params does not exceed maximum allowed;

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -33,17 +33,19 @@ export default async function BlogPage({ params }: { params: { page: number } })
   const postFrontmatter = await getFrontmatter('posts')
   const appearanceSettings = await getEggspressSettings('appearance')
   const numPostsPerPage = appearanceSettings.numberOfPostsPerPage || 8
-  const maxPostIndex = page * numPostsPerPage > postFrontmatter.length ? postFrontmatter.length : page * numPostsPerPage 
+
+  const maxPostIndex = page * numPostsPerPage > postFrontmatter.length ? postFrontmatter.length : page * numPostsPerPage
+  const startIndex = page * numPostsPerPage - numPostsPerPage > postFrontmatter.length ? maxPostIndex - numPostsPerPage : page * numPostsPerPage - numPostsPerPage
 
   return (
     <main className="flex flex-wrap">
       <div className={`hero bleed-${appearanceSettings.colorLightPrimary} dark:bleed-${appearanceSettings.colorDarkPrimary}`}>
         <h1 className="text-5xl font-bold mb-4 -ml-0.5">Posts <span className="text-gray-400 dark:text-gray-500">{`//`} Page {page}</span></h1>      
-        <div className="font-normal">Displaying {page * numPostsPerPage - numPostsPerPage + 1} - {maxPostIndex} of {postFrontmatter.length}</div>
+        <div className="font-normal">Displaying {startIndex + 1} - {maxPostIndex} of {postFrontmatter.length}</div>
       </div>
       <div className="flex justify-between w-full">
         <div className='lg:max-w-prose'>
-          {postFrontmatter.slice(page * numPostsPerPage - numPostsPerPage, maxPostIndex).map((frontmatter, index) => 
+          {postFrontmatter.slice(startIndex, maxPostIndex).map((frontmatter, index) => 
             <PostCard key={`${frontmatter.slug}-${index}`} post={frontmatter}></PostCard>
           )}
         </div>
@@ -53,7 +55,7 @@ export default async function BlogPage({ params }: { params: { page: number } })
       </div>
         <div className="py-12">
           <div className="font-light text-sm mb-2 text-gray-800 dark:text-gray-100">
-            Displaying posts {page * numPostsPerPage - numPostsPerPage + 1} - {maxPostIndex} of {postFrontmatter.length}
+            Displaying posts {startIndex + 1} - {maxPostIndex} of {postFrontmatter.length}
           </div>
           <div className="flex">
             {page > 1 &&
