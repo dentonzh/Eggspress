8e3478ef44c103af13aec277882f1ca525a0730a
dentonzh <denzix@gmail.com>
2023-11-23 19:11:31 -0500

    Add prebuild script to move user-provided assets to production folders

---
+++ app/scripts/loadAssets.js
@@ -0,0 +1,18 @@
+const fs = require('fs-extra')
+
+assetsMap = {
+  'icon.png': 'app/',
+  'logo.png': 'public/assets/',
+  'og_logo.png': 'public/assets/',
+}
+
+sourceDir = 'my_settings/'
+
+Object.keys(assetsMap).map((file) => {
+  const sourceFile = `${sourceDir}${file}`
+  const destinationFile = `${assetsMap[file]}${file}`
+
+  if (fs.existsSync(sourceFile)) {
+    fs.copySync(sourceFile, destinationFile)
+  }
+})
