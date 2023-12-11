d1a8d8cd631010ecab8cb1fd3c14c5b1ae29f90e
dentonzh <denzix@gmail.com>
2023-12-11 15:55:39 -0500

    Wrap function definition for processByLine into if statement to check if fileStream is undefined

--- app/scripts/loadAssets.js
+++ app/scripts/loadAssets.js
@@ -4,25 +4,27 @@ const readline = require('readline')
 const processByLine = async (path) => {
   try {
     const fileStream = fs.createReadStream(path)
+    if (fileStream) {
+      const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+      })
+    
+      for await (const line of rl) {
+        if (line.startsWith('fontFamily:')) {
+          const fontFamily = line.slice(line.indexOf(':') + 1).replaceAll('"', '').replaceAll("'", "").trim().replaceAll(' ', '_')
+          if ( fontFamily ) {
+            fs.writeFileSync(
+              'app/_components/UserFont.tsx',
+              `import {${fontFamily}} from 'next/font/google'\nconst font = ${fontFamily}({ subsets: ['latin'], })\nexport default font`
+            )
+          }
+          break
+        }
+      }
+    }
   } catch { return } // skip setting custom font if settings are not available, typically during setup
 
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  })
-
-  for await (const line of rl) {
-    if (line.startsWith('fontFamily:')) {
-      const fontFamily = line.slice(line.indexOf(':') + 1).replaceAll('"', '').replaceAll("'", "").trim().replaceAll(' ', '_')
-      if ( fontFamily ) {
-        fs.writeFileSync(
-          'app/_components/UserFont.tsx',
-          `import {${fontFamily}} from 'next/font/google'\nconst font = ${fontFamily}({ subsets: ['latin'], })\nexport default font`
-        )
-      }
-      break
-    }
-  }
 }
 
 processByLine('my_settings/appearance.md')
