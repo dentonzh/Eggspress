9507694827242b5aec76468ccbc26b9e0e6ea6a9
dentonzh <denzix@gmail.com>
2023-12-13 20:41:27 -0500

    Remove "bg-opacity-50" from nav menu shadow and add opacity modifier to default appearance.md for color keys targeting the shadow background by affixing "/50"; update appearance.md to include notes about setting opacity in color fields; update eggspress_starter_workspace.zip with new appearance.md

--- app/_components/NavigationMenu.tsx
+++ app/_components/NavigationMenu.tsx
@@ -11,7 +11,7 @@ const NavigationMenu = async ({children}: NavigationMenuProps) => {
 
   return (
     <div className={`font-medium ${appearanceSettings.colorNavMenuTextDark ? `dark:text-${appearanceSettings.colorNavMenuTextDark}` : 'dark:text-gray-200'} ${appearanceSettings.colorNavMenuTextLight ? `text-${appearanceSettings.colorNavMenuTextLight}` : 'text-gray-800'}`}>
-      <div className={`rounded-lg bg-opacity-50 ${appearanceSettings.colorNavMenuBackgroundShadowLight ? 'bg-' + appearanceSettings.colorNavMenuBackgroundShadowLight : 'bg-gray-500'} ${appearanceSettings.colorNavMenuBackgroundShadowDark ? 'dark:bg-' + appearanceSettings.colorNavMenuBackgroundShadowDark : 'dark:bg-gray-500'}`}>
+      <div className={`rounded-lg ${appearanceSettings.colorNavMenuBackgroundShadowLight ? 'bg-' + appearanceSettings.colorNavMenuBackgroundShadowLight : 'bg-gray-500'} ${appearanceSettings.colorNavMenuBackgroundShadowDark ? 'dark:bg-' + appearanceSettings.colorNavMenuBackgroundShadowDark : 'dark:bg-gray-500'}`}>
         <div className={`rounded-lg relative -top-1 -right-1 -left-1 -bottom-1 border border-gray-500 ${appearanceSettings.colorNavMenuBackgroundLight ? 'bg-' + appearanceSettings.colorNavMenuBackgroundLight : 'bg-white'} ${appearanceSettings.colorNavMenuBackgroundDark ? 'dark:bg-' + appearanceSettings.colorNavMenuBackgroundDark : 'dark:bg-slate-800'}`}>
           {children}
         </div>
