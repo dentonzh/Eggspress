7c79f55bf7be681a930c278e0452252d37d39759
dentonzh <denzix@gmail.com>
2023-12-17 23:25:38 -0500

    Implement navigation bar brand text

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -3,18 +3,25 @@ import React from 'react'
 import Logo from '../../public/assets/logo.png'
 import Image from 'next/image'
 import DarkModeToggle from './DarkModeToggle'
-import { getColors } from '../utils'
+import { getColors, getEggspressSettings } from '../utils'
 import DropdownMenu from './DropdownMenu'
 import SiteLinks from './SiteLinks'
 import NavigationMenu from './NavigationMenu'
 
 const Navigation = async () => {
+  const appearanceSettings = await getEggspressSettings('appearance')
+
   return (
     <nav className={`sticky top-0 px-3 md:px-0 duration-200 ${await getColors('bg', 'ThemeNav', 'slate-900', 'gray-100')} py-2 z-10`}>
       <div className="flex container justify-between">
-        <div>
-          <Link href='/'>
-            <Image src={Logo} height={32} style={{width: 'auto', height: 'auto'}} alt='Eggspress blog logo' className='dark:hue-rotate-270 dark:brightness-[3]' />
+        <div className="flex">
+          <Link className="flex items-center" href='/'>
+            {appearanceSettings.showBrandLogo && 
+              <Image src={Logo} height={32} style={{width: 'auto', height: 'auto'}} alt='Eggspress blog logo' className='dark:hue-rotate-270 dark:brightness-[3] mr-2' />
+            }
+            {appearanceSettings.showBrandText &&
+              <span className={`flex font-bold ${await getColors('text', 'NavBarBrandText', 'gray-200', 'gray-800')}`}>{appearanceSettings.brandText}</span>
+            }
           </Link>
         </div>
         <div className="flex items-center dark:text-white">
