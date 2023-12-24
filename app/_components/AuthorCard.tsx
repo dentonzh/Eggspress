537b1a53fd63e265c82fbefa26e6f648209ff57e
Denton Zhou <dxz35@case.edu>
2023-12-24 14:15:20 -0500

    Feature: Content Border and Author Card Settings (#50)
    * Add ContentBlockquoteBorder and ContentTableBorder appearance.md values to loadAssets.js prebuild script
    
    * Add showAuthorCardDescription and showAuthorCardLinks to appearance.md; update AuthorCard component to read these new fields; adjust spacing on AuthorCard elements
    
    * Add keys showAuthorCardProfileImage and showAuthorCardProfileImageOnRight to appearance.md and implement them in AuthorCard component
    
    * Update eggspress_starter_workspace.zip with new default appearance.md
    
    * Slightly increase margin-bottom on author name in AuthorCard

--- app/_components/AuthorCard.tsx
+++ app/_components/AuthorCard.tsx
@@ -31,56 +31,62 @@ const AuthorCard = async ({slug}: {slug: string | null}) => {
 
   return (
     <div className={`mb-16 ${await getColors('text', 'AuthorCardText', 'gray-200', 'gray-600')}`}>
-      <Link href={`/author/${slug}`} className="mb-1 flex flex-wrap">
-        {imageUrl && imageUrl.length > 0 ? (
+      <Link href={`/author/${slug}`} className={`mb-1 flex ${appearanceSettings.showAuthorCardProfileImageOnRight ? 'flex-row-reverse' : 'flex-row'}`}>
+        {(appearanceSettings.showAuthorCardProfileImage === undefined || appearanceSettings.showAuthorCardProfileImage) && imageUrl && imageUrl.length > 0 ? (
           <div className={`${imageUrl.length ? '' : 'hidden'} lg:-ml-2 mr-3 h-11 w-11 rounded-full object-cover overflow-hidden`}>
             <Image src={imageUrl} width="56" height="56" alt={`Profile image for ${authorData.name}`}></Image>
           </div>
-        ) :
-          <div className="-ml-2 mr-3 h-11 w-11 p-2 rounded-full bg-gray-200 dark:bg-gray-600 duration-150">
-            <Image src="/assets/egg.svg" width={96} height={96} alt={`Profile image for ${authorData.name}`}></Image>
+        ) : (
+          <div>
+            {(appearanceSettings.showAuthorCardProfileImage === undefined || appearanceSettings.showAuthorCardProfileImage) &&
+              <div className="-ml-2 mr-3 h-11 w-11 p-2 rounded-full bg-gray-200 dark:bg-gray-600 duration-150">
+                <Image src="/assets/egg.svg" width={96} height={96} alt={`Profile image for ${authorData.name}`}></Image>
+              </div>
+            }
           </div>
-        }
-        <div className={`font-medium my-auto ${await getColors('text', 'AuthorCardHeading')}`}>
-          <span className={authorData.role ? '' : 'pl-2 font-semibold'}>
+        )}
+        <div className={`font-medium my-auto ${await getColors('text', 'AuthorCardHeading')} ${appearanceSettings.showAuthorCardProfileImageOnRight ? 'mr-auto' : ''}`}>
+          <div className={`mb-0.5 ${authorData.role ? '' : 'pl-2 font-semibold'}`}>
             {authorData.name || slug}
-          </span>
+          </div>
           {authorData.role && (<div className="text-xs">{authorData.role}</div>)}
         </div>
       </Link>
-      { authorData.description &&
-        <div className="text-sm py-2 mb-1">
+      {(appearanceSettings.showAuthorCardDescription === undefined || appearanceSettings.showAuthorCardDescription) && authorData.description &&
+        <div className="text-sm py-2 mt-3">
           {authorData.description}
         </div>
       }
-      <div className="flex flex-wrap">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(async (index) => {return (authorData['socialLink' + index] &&
-          <div className={`text-sm w-1/2 md:w-full mb-4 md:mb-2 ${await getColors('text', 'AuthorCardLinkLabel', 'gray-400', 'gray-500')}`}>
-            <span className="pr-1 block md:inline-block">
-              {authorData['socialPlatform' + index] && authorData['socialHandle' + index] ? `${authorData['socialPlatform' + index]}: ` : 'Social: '}
-            </span>
-            <a 
-              href={authorData['socialLink' + index]} 
-              target="_blank" 
-              rel="nofollow noopener" 
-              className={`underline-animated underline-dotted ${await getColors('text', 'AuthorCardLinkText', 'gray-400', 'gray-700')} ${await getColors('hover:text', 'AuthorCardLinkTextHover', 'gray-300', 'gray-800')}`}
-            >
-              {authorData['socialPlatform' + index] && authorData['socialHandle' + index] ? `@${authorData['socialHandle' + index].replace('@', '')}` : authorData['socialPlatform' + index]}
-              {(!authorData['socialPlatform' + index] || !authorData['socialPlatform' + index].length) ? authorData['socialLink' + index].slice(authorData['socialLink' + index].lastIndexOf('://')+3) : '' }
-            </a>
-          </div>
-        )})}
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(async (index) => {return (authorData['websiteLink' + index] &&
-          <div className={`text-sm w-1/2 md:w-full mb-4 md:mb-2 ${await getColors('text', 'AuthorCardLinkLabel', 'gray-400', 'gray-500')}`}>
-            <span className="pr-1 block md:inline-block">
-              {authorData['websiteLabel' + index] || 'Website'}:
-            </span>
-            <a href={authorData['websiteLink' + index]} target="_blank" rel="" className={`underline-animated underline-dotted ${await getColors('text', 'AuthorCardLinkText', 'gray-400', 'gray-700')} ${await getColors('hover:text', 'AuthorCardLinkTextHover', 'gray-300', 'gray-800')}`}>
-              {authorData['websiteName' + index] ? authorData['websiteName' + index] : authorData['websiteLink' + index].slice(authorData['websiteLink' + index].lastIndexOf('://')+3)}
-            </a>
-          </div>
-        )})}
-      </div>
+      {(appearanceSettings.showAuthorCardLinks === undefined || appearanceSettings.showAuthorCardLinks) && 
+        <div className="flex flex-wrap mt-3">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(async (index) => {return (authorData['socialLink' + index] &&
+            <div className={`text-sm w-1/2 md:w-full mb-4 md:mb-2 ${await getColors('text', 'AuthorCardLinkLabel', 'gray-400', 'gray-500')}`}>
+              <span className="pr-1 block md:inline-block">
+                {authorData['socialPlatform' + index] && authorData['socialHandle' + index] ? `${authorData['socialPlatform' + index]}: ` : 'Social: '}
+              </span>
+              <a 
+                href={authorData['socialLink' + index]} 
+                target="_blank" 
+                rel="nofollow noopener" 
+                className={`underline-animated underline-dotted ${await getColors('text', 'AuthorCardLinkText', 'gray-400', 'gray-700')} ${await getColors('hover:text', 'AuthorCardLinkTextHover', 'gray-300', 'gray-800')}`}
+              >
+                {authorData['socialPlatform' + index] && authorData['socialHandle' + index] ? `@${authorData['socialHandle' + index].replace('@', '')}` : authorData['socialPlatform' + index]}
+                {(!authorData['socialPlatform' + index] || !authorData['socialPlatform' + index].length) ? authorData['socialLink' + index].slice(authorData['socialLink' + index].lastIndexOf('://')+3) : '' }
+              </a>
+            </div>
+          )})}
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(async (index) => {return (authorData['websiteLink' + index] &&
+            <div className={`text-sm w-1/2 md:w-full mb-4 md:mb-2 ${await getColors('text', 'AuthorCardLinkLabel', 'gray-400', 'gray-500')}`}>
+              <span className="pr-1 block md:inline-block">
+                {authorData['websiteLabel' + index] || 'Website'}:
+              </span>
+              <a href={authorData['websiteLink' + index]} target="_blank" rel="" className={`underline-animated underline-dotted ${await getColors('text', 'AuthorCardLinkText', 'gray-400', 'gray-700')} ${await getColors('hover:text', 'AuthorCardLinkTextHover', 'gray-300', 'gray-800')}`}>
+                {authorData['websiteName' + index] ? authorData['websiteName' + index] : authorData['websiteLink' + index].slice(authorData['websiteLink' + index].lastIndexOf('://')+3)}
+              </a>
+            </div>
+          )})}
+        </div>
+      }
     </div>
   )
 }
