ecaa952a36d1c1d26316556886346fce6ac8c725
dentonzh <denzix@gmail.com>
2023-12-23 20:28:50 -0500

    Update h2, h3, h4, h5 in globals.css to use an html trick using pseudo-element ::before to create an offset, allowing Toc links to scroll to the correct position (previously, scrolled too far with nav hiding start of content); Update EggspressLink component to pass through id property, fixing an issue where references from footnotes to the referencing footnote placed readers at the top of the page (because anchor links on footnote reference did not have id)

--- app/_components/Toc.tsx
+++ app/_components/Toc.tsx
@@ -40,7 +40,7 @@ const Toc = () => {
 
   const scrollToSection = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, elementId: string) => {
     e.preventDefault()
-    document.getElementById(elementId)?.scrollIntoView({behavior: "smooth", block: "center"})
+    document.getElementById(elementId)?.scrollIntoView({behavior: "smooth"})
     if (elementId === 'mobile-toc') {
       clearTimeout(returnToTopTimer)
       setShowReturnToTop(false)
