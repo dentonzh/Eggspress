e307139cc82c6295a1b5004eabeb68102073f409
dentonzh <denzix@gmail.com>
2024-01-08 10:02:58 -0500

    Create EggspressTable component
    EggspressTable will be used to compile content when calling compileMDX

---
+++ app/_components/EggspressTable.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface EggspressTableProps {
+  children: React.ReactNode
+}
+
+const EggspressTable: React.FC<EggspressTableProps> = async ({children}: EggspressTableProps) => {
+  return (
+    <div className="overflow-x-auto">
+      <table>
+        {children}
+      </table>
+    </div>
+  )
+}
+
+export default EggspressTable
