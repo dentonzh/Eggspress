3be8d1262a215880ec04e3fbc79245315c68a8f7
dentonzh <denzix@gmail.com>
2024-01-11 10:59:06 -0500

    Add JS to ShareSocialButton and ShareXButton
    On click, users will be taken to a new pop up window containing sharing page of a respective social media site; Fixes a bug in previous commit where URL may contain "undefined" if urlSuffix not provided to ShareSocialButton

--- app/_components/ShareSocialButton.tsx
+++ app/_components/ShareSocialButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { usePathname } from "next/navigation"
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 
 type ShareSocialButtonProps = {
   className?: string,
@@ -10,14 +10,20 @@ type ShareSocialButtonProps = {
   children: ReactNode
 }
 
-
-
 const ShareSocialButton = ({className, baseUrl, skipEncode, urlSuffix, children}: ShareSocialButtonProps) => {
+  const [clicked, setClicked] = useState(false)
+
   const pathname = usePathname()
   const url = window.location.origin + pathname
+  const href =`${baseUrl}${skipEncode ? url : encodeURIComponent(url)}${urlSuffix || ''}`
 
   return (
-    <a className={className} target="_blank" href={`${baseUrl}${skipEncode ? url : encodeURIComponent(url)}${urlSuffix}`}>
+    <a 
+      className={className} 
+      target="_blank" 
+      onClick={() => {if (!clicked) {window.open(href,'targetWindow','toolbar=no,location=0,status=no,menubar=no,scrollbars=yes,resizable=yes,width=600,height=500'); setClicked(true); setTimeout(() => {setClicked(false)}, 500)};}}
+      href={href}
+    >
       {children}
     </a>
   )
