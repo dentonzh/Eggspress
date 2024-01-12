ac9a2c505d9faeabc364275074f43e0b137d7992
dentonzh <denzix@gmail.com>
2024-01-12 15:37:53 -0500

    Improve spacing in ShareMenu
    Reduce padding-x on sm or larger viewports for ShareMenu; Increase padding-right for Facebook and WhatsApp share buttons

--- app/_components/ShareMenu.tsx
+++ app/_components/ShareMenu.tsx
@@ -23,7 +23,7 @@ const ShareMenu = async ({className, align, headline, subtitle, siteName}: Share
     <div className={className}>
       <DropdownMenu text={appearanceSettings.shareText ? appearanceSettings.shareText : 'Share'} align={align || 'left-0'}>
         <NavigationMenu>
-          <div className="min-w-[10rem] sm:min-w-[12rem] px-3 pt-6 pb-3">
+          <div className="min-w-[10rem] sm:min-w-[12rem] px-3 sm:px-2 pt-6 pb-3">
             <div className="mb-3">
               {(appearanceSettings.showShareByLinkButton === undefined || appearanceSettings.showShareByLinkButton) && 
                 <ShareCopyButton className="dropdown-item pl-2 sm:px-3 py-2 block">
@@ -56,7 +56,7 @@ const ShareMenu = async ({className, align, headline, subtitle, siteName}: Share
             </div>
             <div className="mb-3">
               {(appearanceSettings.showShareByFacebookButton === undefined || appearanceSettings.showShareByFacebookButton) && 
-                <ShareSocialButton className="dropdown-item pl-2 sm:pl-3 sm:pr-6 py-2 block" baseUrl={"https://www.facebook.com/sharer.php?u="}>
+                <ShareSocialButton className="dropdown-item pl-2 sm:pl-3 sm:pr-8 py-2 block" baseUrl={"https://www.facebook.com/sharer.php?u="}>
                   <Image className="text-gray-500 inline-block dark:brightness-150" src="/assets/facebook.svg" width={16} height={16} alt="share on facebook icon"></Image>
                   <span className="pl-2 sm:pl-4"><span className="hidden sm:inline">
                     {`${appearanceSettings.shareOnText ? appearanceSettings.shareOnText : 'Share on'} `}
@@ -106,7 +106,7 @@ const ShareMenu = async ({className, align, headline, subtitle, siteName}: Share
             </div>  
             <div className="mb-3">
               {(appearanceSettings.showShareByWhatsappButton) && 
-                <ShareSocialButton className="dropdown-item pl-2 sm:pl-3 sm:pr-7 py-2 block" windowWidth={1024} baseUrl={"https://api.whatsapp.com/send?text="}>
+                <ShareSocialButton className="dropdown-item pl-2 sm:pl-3 sm:pr-8 py-2 block" windowWidth={1024} baseUrl={"https://api.whatsapp.com/send?text="}>
                   <Image className="text-gray-500 inline-block dark:brightness-150" src="/assets/whatsapp.svg" width={16} height={16} alt="share on whatsapp icon"></Image>
                   <span className="pl-2 sm:pl-4"><span className="hidden sm:inline">
                     {`${appearanceSettings.shareOnText ? appearanceSettings.shareOnText : 'Share on'} `}
