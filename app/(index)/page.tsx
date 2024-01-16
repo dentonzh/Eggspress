fbd1512337a36cc40c55f62f925a120b4e80c6c4
dentonzh <denzix@gmail.com>
2024-01-15 23:28:14 -0500

    Remove "font-light" class name from index page pagination

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -50,7 +50,7 @@ export default async function Home() {
           }
           {postFrontmatter.length > (appearanceSettings.numberOfPostsPerPage || 8) &&
             <div className="my-12">
-              <div className="font-light text-sm mb-2 text-gray-800 dark:text-gray-100">
+              <div className="text-sm mb-2 text-gray-800 dark:text-gray-100">
                 {await getString('paginationRangePrefix', 'Displaying posts ')}1 - {appearanceSettings.numberOfPostsPerPage || 8}{await getString('paginationRangeSuffix', '')}{await getString('paginationTotalCountPrefix', ' of ')}{postFrontmatter.length}{await getString('paginationTotalCountSuffix', '')}
               </div>
               <PaginationLink text={await getString('showMorePostsButtonLabel', 'Show more posts')} page={2}></PaginationLink>
