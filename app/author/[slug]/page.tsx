7b6e4e5d77834b31029be70566f37bdb92542414
dentonzh <denzix@gmail.com>
2024-01-21 10:32:31 -0500

    Add a max width to ContentHero text sections
    Set content className to max-w-[74ch] and add className "ml-auto" to image section of ContentHero

--- app/author/[slug]/page.tsx
+++ app/author/[slug]/page.tsx
@@ -61,9 +61,7 @@ const AuthorPage =  async ( {params}: {params: {slug: string}} ) => {
 
   const postFrontmatter = await getFrontmatter('posts', (frontmatter && frontmatter.orderPostsBy) || appearanceSettings.orderPostsInAuthorsBy, (frontmatter && frontmatter.orderPostsByReversed) || appearanceSettings.orderPostsInAuthorsByReversed)
   const filteredPosts = postFrontmatter.filter(fm => fm.author === slug || fm.author?.split(',').map(x => x.trim()).includes(slug))
-
   const imageSrc = frontmatter && frontmatter.image ? await getProfileImage(frontmatter.image) : ''
-
   const sections = ['pronouns', 'location', 'education', 'degree', 'work', 'company', 'title', 'specialty', 'team']
 
   if (!frontmatter) {
